e368e7a827778a580d2abe513ed54bdd3164f1db
SazuChan <kevin.hoang@viacesi.fr>
2020-05-11 19:00:39 +0200

    last before reset

--- src/components/Kanban.js
+++ src/components/Kanban.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
 import { useAuth0 } from "../react-auth0-spa";
-import styles from './Profile.module.css';
+import styles from './Kanban.module.css';
 
 class Kanban extends React.Component {
     render() {
         return (
             <div>
-                Oui
+                Kanban
             </div>
         );
       }
